be8537404d9420e0231c564274602938ab9fc210
animeroman <163325219+animeroman@users.noreply.github.com>
2024-12-17 11:00:48 +0400

    Updating config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,6 @@
-export const endpoint =
-  'https://animeroman.github.io/Source/json/main-search.json';
+export const endpoint = 'https://apiromanlast.fly.dev/api/anime';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const apiKey =
+  'SkYCKXd3lZwgW7SDZZBcQOkHoCw4ggczeGFAmtbdUeJFTMWua3KYW9RDw36Esppx1c6Kp6wfy0fTh1YdvUTMF5faEyurPItvRwUKrkiZtT8DMO33yiHEppNcusg85dYC'; // Use the same key as in the backend
+export const MODAL_CLOSE_SEC = 2.5;
