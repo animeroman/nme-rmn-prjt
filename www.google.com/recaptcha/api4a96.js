2645383626e5883deb8568653d90ee637ca1cb39
animeroman <163325219+animeroman@users.noreply.github.com>
2025-01-07 15:09:20 +0400

    Removing some logs

--- www.google.com/recaptcha/api4a96.js
+++ www.google.com/recaptcha/api4a96.js
@@ -1,34 +1,33 @@
 /* PLEASE DO NOT COPY AND PASTE THIS CODE. */ (function () {
   var w = window,
-    C = "___grecaptcha_cfg",
+    C = '___grecaptcha_cfg',
     cfg = (w[C] = w[C] || {}),
-    N = "grecaptcha";
+    N = 'grecaptcha';
   var gr = (w[N] = w[N] || {});
   gr.ready =
     gr.ready ||
     function (f) {
-      (cfg["fns"] = cfg["fns"] || []).push(f);
+      (cfg['fns'] = cfg['fns'] || []).push(f);
     };
-  w["__recaptcha_api"] = "https://www.google.com/recaptcha/api2/";
-  (cfg["render"] = cfg["render"] || []).push(
-    "6Lc7dH8pAAAAAIGw-BOEYDAZvcs3afxf6XHaLsQL",
+  w['__recaptcha_api'] = 'https://www.google.com/recaptcha/api2/';
+  (cfg['render'] = cfg['render'] || []).push(
+    '6Lc7dH8pAAAAAIGw-BOEYDAZvcs3afxf6XHaLsQL'
   );
-  w["__google_recaptcha_client"] = true;
+  w['__google_recaptcha_client'] = true;
   var d = document,
-    po = d.createElement("script");
-  po.type = "text/javascript";
+    po = d.createElement('script');
+  po.type = 'text/javascript';
   po.async = true;
-  po.charset = "utf-8";
-  po.src =
-    "https://www.gstatic.com/recaptcha/releases/hfUfsXWZFeg83qqxrK27GB8P/recaptcha__en.js";
-  po.crossOrigin = "anonymous";
+  po.charset = 'utf-8';
+  po.src = '';
+  po.crossOrigin = 'anonymous';
   po.integrity =
-    "sha384-CUWl3AwpNax4J2/ffIDxMr+sLnEFg8pkj5QqZtMtJZtrL5bS5QTkIoza4qZakWjL";
-  var e = d.querySelector("script[nonce]"),
-    n = e && (e["nonce"] || e.getAttribute("nonce"));
+    'sha384-CUWl3AwpNax4J2/ffIDxMr+sLnEFg8pkj5QqZtMtJZtrL5bS5QTkIoza4qZakWjL';
+  var e = d.querySelector('script[nonce]'),
+    n = e && (e['nonce'] || e.getAttribute('nonce'));
   if (n) {
-    po.setAttribute("nonce", n);
+    po.setAttribute('nonce', n);
   }
-  var s = d.getElementsByTagName("script")[0];
+  var s = d.getElementsByTagName('script')[0];
   s.parentNode.insertBefore(po, s);
 })();
